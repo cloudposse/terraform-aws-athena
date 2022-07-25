25deb5b71c45ff685c9ebe66015007242576a5bb
Dan Meyers <danjbh@users.noreply.github.com>
2022-07-25 11:25:54 -0700

    More fixes for terratest

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,19 @@
 output "s3_bucket_id" {
   description = "ID of S3 bucket used by Athena."
-  value       = aws_s3_bucket.default[0].id
+  value       = try(aws_s3_bucket.default[0].id, null)
 }
 
 output "kms_key_arn" {
   description = "ARN of KMS key used by Athena."
-  value       = aws_kms_key.default[0].arn
+  value       = try(aws_kms_key.default[0].arn, null)
 }
 
 output "workgroup_arn" {
   description = "ARN of newly created Athena workgroup."
-  value       = aws_athena_workgroup.default[0].arn
+  value       = try(aws_athena_workgroup.default[0].arn, null)
 }
 
 output "database_id" {
   description = "ID of newly created Athena database."
-  value       = aws_athena_database.default[0].id
+  value       = try(aws_athena_database.default[0].id, null)
 }
